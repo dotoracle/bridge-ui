9973819b1513112277f5291ff57b92a5201eb809
hka <hoangkianh31592@gmail.com>
2021-06-03 15:49:55 +0700

    fix: selected token

--- src/components/TokenSelect/index.tsx
+++ src/components/TokenSelect/index.tsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { EuiButton } from '@elastic/eui'
 import styled from 'styled-components'
+import UnknownSVG from '../../assets/images/unknown.svg'
+import BridgeAppContext from '../../context/BridgeAppContext'
 import SearchModal from '../SearchModal'
 
 const Label = styled.label`
   display: block;
   margin-bottom: 1rem;
   color: #aeaeb3;
+`
+const TokenLogo = styled.img`
+  width: 24px;
+  height: 24px;
+  border-radius: 100%;
+  margin-right: 0.75rem;
 `
 const SelectButton = styled(EuiButton)`
   width: 100%;
@@ -23,9 +31,14 @@ const SelectButton = styled(EuiButton)`
     box-shadow: 0 4px 8px 0 ${props => props.theme.primary}26, 0 2px 2px -1px ${props => props.theme.primary}4d !important;
     background-color: #32323c !important;
   }
+
+  .euiButton__text {
+    display: flex;
+  }
 `
 
 const TokenSelect = (): JSX.Element => {
+  const { token: selectedToken } = useContext(BridgeAppContext)
   const [isModalVisible, setIsModalVisible] = useState(false)
 
   const closeModal = () => setIsModalVisible(false)
@@ -34,9 +47,16 @@ const TokenSelect = (): JSX.Element => {
   return (
     <>
       <Label>Assets</Label>
-      <SelectButton iconType="arrowRight" iconSide="right" onClick={showModal}>
-        Select a Token
-      </SelectButton>
+      {selectedToken ? (
+        <SelectButton iconType="arrowRight" iconSide="right" onClick={showModal}>
+          <TokenLogo src={selectedToken.logoURI ? selectedToken.logoURI : UnknownSVG} alt={selectedToken.name} />
+          {selectedToken.name}
+        </SelectButton>
+      ) : (
+        <SelectButton iconType="arrowRight" iconSide="right" onClick={showModal}>
+          Select a Token
+        </SelectButton>
+      )}
       {isModalVisible && <SearchModal closeModal={closeModal} />}
     </>
   )
