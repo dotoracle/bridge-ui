9e2b5a70f12e35ba09abfeb2edcec39d2814c3ba
hoangkianh <hoangkianh31592@gmail.com>
2022-06-05 15:39:38 +0700

    chore: remove faucet

--- src/components/Header/items.ts
+++ src/components/Header/items.ts
@@ -5,11 +5,6 @@ const NAV_ITEMS: Array<NavItem> = [
     label: 'Home',
     to: '/',
   },
-  {
-    label: 'Faucet',
-    href: 'https://faucet.dotoracle.network',
-    target: '__blank',
-  },
 ]
 
 export default NAV_ITEMS
